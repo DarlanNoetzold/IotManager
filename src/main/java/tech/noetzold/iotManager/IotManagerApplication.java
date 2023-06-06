e5da648eddccb0e7d70a44ec60bf25777297e13a
DarlanNoetzold <DarlanNoetzold>
2023-06-06 18:24:17 -0300

    init

---
+++ src/main/java/tech/noetzold/iotManager/IotManagerApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.iotManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/noetzold/iotManager/IotManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.iotManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
